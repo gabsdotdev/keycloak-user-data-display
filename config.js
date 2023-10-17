19f461632ead5ddf57d70b54ee29d1db5f95b3f0
Gabriell Amaral <gabriellopes2213@gmail.com>
2023-10-17 18:31:03 -0300

    feat: create an html page to show user information, how to access token, id token and user info

---
+++ config.js
@@ -0,0 +1,8 @@
+var keycloak = {
+    url : 'http://your-keycloak-local:8080/auth',
+    realm: 'your_realm_name',
+    clientId: 'your_client_id',
+    redirectUriEncoder: 'encodeURI',
+    redirectUri: 'http://127.0.0.1:5500/index.html'
+  };
+  
