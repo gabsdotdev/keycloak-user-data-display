820647a95f499ceeeaef1355ddd0441c42009ac0
Gabriell Amaral <gabriellopes2213@gmail.com>
2023-10-20 16:41:42 -0300

    feat: add docker compose with keycloak local

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 var keycloak = {
-    url : 'http://your-keycloak-local:8080/auth',
-    realm: 'your_realm_name',
-    clientId: 'your_client_id',
-    redirectUriEncoder: 'encodeURI',
+    url : 'http://127.0.0.1:8080',
+    realm: 'sandbox',
+    clientId: 'sandbox_client',
+    redirectUriEncoder: 'http%3A%2F%2F127.0.0.1%3A5500%2Findex.html', //You can use this site for encoder your url -> https://www.urlencoder.org
     redirectUri: 'http://127.0.0.1:5500/index.html'
   };
-  
