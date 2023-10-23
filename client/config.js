ecebaf16604fddab7a811782f1eeafa8d70d6b33
Gabriell Amaral <gabriellopes2213@gmail.com>
2023-10-23 12:59:11 -0300

    fix: use the encodeURIComponent method to encode the redirect URI

--- client/config.js
+++ client/config.js
@@ -2,6 +2,5 @@ var keycloak = {
     url : 'http://127.0.0.1:8080', // Set your keycloak base url
     realm: 'sandbox', // Set your realm
     clientId: 'sandbox_client', // Set your clientId
-    redirectUriEncoder: 'http%3A%2F%2F127.0.0.1%3A8000%2Findex.html', //You can use this site for encoder your url -> https://www.urlencoder.org
     redirectUri: 'http://127.0.0.1:8000/index.html'
   };
